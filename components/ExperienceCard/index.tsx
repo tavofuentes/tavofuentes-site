e66e87c722c230889d6ce6a039282897b7cd056a
Octavio Fuentes <tavofuentes@gmail.com>
2024-07-18 14:49:26 -0600

    style: fix exp cards layout

--- components/ExperienceCard/index.tsx
+++ components/ExperienceCard/index.tsx
@@ -29,10 +29,18 @@ const ExperienceCard = ({ experienceRecord, bgColor, imgBgColor }: Props) => {
         rounded-lg
         flex flex-grow flex-col-reverse
         md:flex-row
+        lg:flex-col-reverse
+        xl:flex-row
         outline2
       `}
       >
-        <div className="outline3 p-6 md:p-12 flex-1">
+        <div
+          className="
+          outline3
+          p-6 
+          md:p-12
+          flex-1"
+        >
           <h1
             className={`${fontLiterata.className} text-5xl mb-2 md:mb-3 text-black font-semibold`}
           >
@@ -67,9 +75,11 @@ const ExperienceCard = ({ experienceRecord, bgColor, imgBgColor }: Props) => {
         <div
           className={`${imgBgColor} outline2
           rounded-t-lg
-          md:rounded-tl-none md:rounded-r-lg
+          p-1
           h-64
-          md:h-auto md:w-1/3
+          md:rounded-tl-none md:rounded-r-lg md:h-auto md:w-1/3
+          lg:w-full lg:rounded-t-lg lg:rounded-r-none lg:h-64
+          xl:rounded-tl-none xl:rounded-r-lg xl:h-auto xl:w-1/3 xl:h-auto
           flex justify-center items-center`}
         >
           <Image
